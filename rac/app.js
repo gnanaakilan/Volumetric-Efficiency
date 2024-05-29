f681c68d3bb0709866ba73d82dcc95ce31915c90
gnanaakilan <gnanaakilan93@gmail.com>
2024-05-30 00:05:38 +0530

    update

--- rac/app.js
+++ rac/app.js
@@ -1,20 +1,5 @@
-// let constants = {
-//     dlp : 55,
-//     dhp:50,
-//     l1:65,
-//     n:720,
-//     hp:2,
-//     d:15,
-//     cd:0.62
-// }
 
-// // check in localstorage
-// let localconst = localStorage.getItem("cvalues")
-// if(localconst){
-//     constants = JSON.parse(localconst)
-// }
-
-
+// single acting single  stage 
 let sasss_constants = {
     dlp : 55,
     L :65,
@@ -41,7 +26,7 @@ function update_sasss_constants() {
 }
 
 
-
+// single acting double stage
 let sadss_constants = {
     dlp : 55, 
     dhp :  50,
@@ -63,5 +48,36 @@ function update_sasss_constants() {
         cd : document.querySelector("#cd") .value
     };
     localStorage.setItem("sadss_constants", JSON.stringify(cvalues))
+    alert("saved")
+}
+
+
+
+// double acting single  stage 
+let dasss_constants = {
+    dlp : 290,
+    L :300,
+    bar:8,
+    n:500,
+    c:0.03,
+    pa : 1,
+    ta:15,
+    f:50,
+    hp:1,
+    l : 50
+}
+if(localStorage.getItem("dasss_constants")) dasss_constants = JSON.parse(localStorage.getItem("dasss_constants"))
+
+function update_dasss_constants() {
+    let cvalues = {
+      dlp : document.querySelector("#dlp").value,
+      L : document.querySelector("#L").value,
+      bar : document.querySelector("#bar").value,
+      n : document.querySelector("#n").value,
+      f : document.querySelector("#f").value,
+      hp : document.querySelector("#hp").value,
+      l : document.querySelector("#l").value
+    };
+    localStorage.setItem("dasss_constants", JSON.stringify(cvalues))
     alert("saved")
 }
